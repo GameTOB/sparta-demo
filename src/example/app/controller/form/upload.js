f92f3aeea3ad1294c752b922b94c63eacf6e536e
irideas <irideas@gmail.com>
2015-07-21 16:59:24 +0800

    fix upload

--- src/example/app/controller/form/upload.js
+++ src/example/app/controller/form/upload.js
@@ -2,93 +2,24 @@
 
 angular.module("app")
 
-.controller("app.form.uploadCtrl", function($rootScope , $scope, $timeout, $q , UIAlert , ExampleData) {
+//通用的配置在 app/conf/upload.js
 
-    
+//相关的过滤器在 app/filter/upload.js
+
+.controller("app.form.uploadCtrl", function($rootScope , $scope, $timeout) {
+	//空    
 })
 
-.filter('uploadErrorMessage' , function(){
-	var units = [ 'B', 'K', 'M', 'G', 'TB' ];
-	var formatSize = function( size, pointLength, maxUnit ) {
-            var unit , customUnits = [];
-            if(angular.isUndefined(pointLength)){
-            	pointLength = 2;
-            }
 
-            if(maxUnit){
-            	units.some(function(unit){
-            		customUnits.push(unit);
-            		return maxUnit == unit;
-            	});
-            }else{
-            	customUnits = units;
-            }
-            //提取基础单位B
-            unit = customUnits.shift();
-            while ( customUnits.length > 0 && size >= 1024 ) {
-            	size = size / 1024;
-            	unit = customUnits.shift();
-            }
-
-            return (unit === 'B' ? size : size.toFixed( pointLength )) +
-                    unit;
-     };
-
-	return function(errorType , options , fileCalledName){
-		var errorMessage = "",
-			fileCalledName = fileCalledName || "文件";
-		switch(errorType){
-			case "F_EXCEED_SIZE":
-				errorMessage = fileCalledName+"大小超限，上限为："+formatSize(options.fileSingleSizeLimit,0,'K');
-				break;
-			case "Q_EXCEED_SIZE_LIMIT":
-				errorMessage = fileCalledName+"总大小超限，上限为："+formatSize(options.fileSizeLimit,0,'M');
-				break;
-    		case "Q_EXCEED_NUM_LIMIT":
-    			errorMessage = fileCalledName+"总数量超出，上限为："+options.fileNumLimit+"个";
-    			break;
-    		case "Q_TYPE_DENIED":
-    			errorMessage = fileCalledName+"格式不符合要求";
-    			break;
-	    	case "F_DUPLICATE":
-	    		errorMessage = "你已选过该"+fileCalledName;
-	    		break;
-		}
-		return errorMessage;
-	};
-})
-
-.controller("app.form.uploadAvatarCtrl", function($rootScope , $scope, $timeout, $q , UIAlert , ExampleData) {
+.controller("app.form.uploadAvatarCtrl", function($rootScope , $scope, $timeout) {
 
 	$scope.avatar = "";
 
 	$scope.uploaderOptions = {
-		//auto: true,
-		fileSingleSizeLimit: 1024 * 1024 * 1,
+		//限制单张图片大小
+		fileSingleSizeLimit: 1024 * 1024 * 2,
+		//限制最多上传数
         fileNumLimit : 1,
-        accept: {
-            title: 'Images',
-            extensions: 'gif,jpg,jpeg,png,JPG,GIF,PNG',
-            mimeTypes: 'image/*'
-        },
-        server : "http://oc.zhouyao.dev.ayibang.cn/upload.php",
-        thumb  : {
-            width: 120,
-            height: 120,
-            // 图片质量，只有type为`image/jpeg`的时候才有效。
-            quality: 80,
-
-            // 是否允许放大，如果想要生成小图的时候不失真，此选项应该设置为false.
-            allowMagnify: false,
-
-            // 是否允许裁剪。
-            crop: true,
-
-            // 为空的话则保留原有图片格式。
-            // 否则强制转换成指定的类型。
-            type: 'image/jpeg'
-        },
-        chunked : false
     };
 
    	$scope.errorType = "";
@@ -119,41 +50,21 @@ angular.module("app")
 
     $scope.$on("avatarUploader::uploadSuccess" , function($event , $uploader , file , res){
     	console.log("uploder::uploadSuccess" , res);
+    	//此时可以保存头像...等等
     	$scope.avatar = res.result;
     })
 
 })
 
-.controller("app.form.uploadPhotosCtrl", function($rootScope , $scope, $timeout, $q , $filter , UIAlert , ExampleData) {
+.controller("app.form.uploadPhotosCtrl", function($rootScope , $scope, $timeout, $filter) {
 	$scope.photos = [];
 	$scope.errorMessage = "";
 	$scope.uploaderOptions = {
+		//限制上传的所有图片大小
 		fileSizeLimit : 10*1024*1024,
+		//限制上传的图片个数上限
         fileNumLimit : 3,
-        accept: {
-            title: 'Images',
-            extensions: 'gif,jpg,jpeg,png,JPG,GIF,PNG',
-            mimeTypes: 'image/*'
-        },
-        server : "http://oc.zhouyao.dev.ayibang.cn/upload.php",
-        thumb  : {
-            width: 120,
-            height: 120,
-            // 图片质量，只有type为`image/jpeg`的时候才有效。
-            quality: 80,
-
-            // 是否允许放大，如果想要生成小图的时候不失真，此选项应该设置为false.
-            allowMagnify: false,
-
-            // 是否允许裁剪。
-            crop: true,
-
-            // 为空的话则保留原有图片格式。
-            // 否则强制转换成指定的类型。
-            type: 'image/jpeg'
-        }
     };
-
     
     var showError = (function(){
     	var hasTimer = false;
--- src/example/app/template/form/upload.md
+++ src/example/app/template/form/upload.md
@@ -3,10 +3,10 @@
 <div class="col-xs-12">
 	<div class="ibox" ng-controller="app.form.uploadAvatarCtrl">
 		<div class="ibox-title">
-			选取头像的案例
+			选取头像的案例 (只能1张 , 重新选取可自动替换上次所选)
 		</div>
 		<div class="ibox-content">
-			<div ng-if="avatar">已选头像地址:<a href="{{avatar}}" target="_blank">{{avatar}}</a></div>
+			<div class="alert alert-success" ng-if="avatar">您已保存完头像 ，可继续其他操作</div>
 			<div name="avatarUploader" uploader-init="uploaderOptions">
 				<p ng-if="errorType" class="alert alert-warning">{{errorType | uploadErrorMessage : $uploader.options}} , 这里用 errorType:{{errorType}} 实现只是模板上用filter的举例而已</p> 
 				<p ng-if="$uploader.files.length > 0">
@@ -15,6 +15,7 @@
 					alt="{{$uploader.files[0].name}}" />
 					<button class="btn btn-danger fa fa-times" ng-if="$uploader.files[0].getStatus()=='inited'" ng-click="$uploader.removeFile($uploader.files[0])"></button>
 					<button class="btn btn-success fa fa-check" ng-if="$uploader.files[0].getStatus()=='complete'" disabled></button>
+					<p ng-if="$uploader.files[0].getStatus()=='complete'">已上传 ，<a href="{{avatar}}" target="_blank">点此查看</a></p>
 				</p>
 
 				<p>
@@ -29,18 +30,18 @@
 			    ng-click="$uploader.upload();" 
 			    ng-disabled="$uploader.isInProgress()" 
 			    ng-switch="$uploader.isInProgress()">
-					<span ng-switch-default>上传</span>
-                    <span ng-switch-when="true">上传中...</span>
+					<span ng-switch-default>保存</span>
+                    <span ng-switch-when="true">保存中...</span>
 			    </button></p>
 			</div>
 		</div>
 	</div>
 	<div class="ibox" ng-controller="app.form.uploadPhotosCtrl">
 		<div class="ibox-title">
-			选取照片案例
+			选取照片案例 (至少2张，最多3张)
 		</div>
 		<div class="ibox-content">
-			<div ng-if="photos.length>0">已上传照片:<a ng-repeat="photo in photos" href="{{photo}}" target="_blank">{{photo}}</a></div>
+			<div class="alert alert-success" ng-if="photos.length>0">您已上传完毕照片 ，可继续其他操作</div>
 			<div name="photosUploader" class="row" uploader-init="uploaderOptions">
 				<p ng-if="errorMessage" class="alert alert-warning">{{errorMessage}}</p>
 				<div class="col-xs-3" ng-repeat="file in $uploader.files">
@@ -50,13 +51,10 @@
 				      <div class="caption">
 				        <p>
 				        	<button class="btn btn-danger fa fa-times" ng-if="file.getStatus()=='inited'" ng-click="$uploader.removeFile(file)"></button>
-				        	<button class="btn btn-success fa fa-check" ng-if="file.getStatus()=='complete'" disabled></button>
 				        </p>
+				        <p ng-if="file.getStatus()=='complete'">已上传 ，<a href="{{photos[$index]}}" target="_blank">点此查看</a></p>
 				      </div>
 				    </div>
-				</div>
-				<div style="height:100px;border:1px solid #ccc;display:table;" class="col-xs-8 row col-xs-offset-2" uploader-dnd uploader-paste>
-					<p style="display:table-cell;text-align:center;vertical-align:middle;">请拖拽照片至此</p>
 				</div>
 				<div class="col-xs-12">
 				<button 
